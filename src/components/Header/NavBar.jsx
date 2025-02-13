8d2a3614e75abf3283848d8cd1fb13bb9352a7ab
Shaukat Ali <Shaukatalibans@gmail.com>
2025-02-13 13:28:32 +0500

    Move navlinks details to array of objects

--- src/components/Header/NavBar.jsx
+++ src/components/Header/NavBar.jsx
@@ -1,6 +1,13 @@
 import { NavLink } from "react-router-dom";
 import { SearchField } from "./SearchField";
 
+const navLinks = [
+    { title: "Home", path: "/" },
+    { title: "Popular", path: "/movies/popular" },
+    { title: "Top Rated", path: "/movies/top" },
+    { title: "Upcoming", path: "/movies/upcoming" },
+];
+
 export const NavBar = ({ open, onSearch }) => {
     const activeNav =
         "block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
@@ -15,47 +22,18 @@ export const NavBar = ({ open, onSearch }) => {
         >
             <SearchField onSearch={onSearch} className="mt-4 md:hidden" />
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                    <NavLink
-                        to="/"
-                        className={({ isActive }) =>
-                            isActive ? activeNav : inactiveNav
-                        }
-                        aria-current="page"
-                    >
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/movies/popular"
-                        className={({ isActive }) =>
-                            isActive ? activeNav : inactiveNav
-                        }
-                    >
-                        Popular
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/movies/top"
-                        className={({ isActive }) =>
-                            isActive ? activeNav : inactiveNav
-                        }
-                    >
-                        Top Rated
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to="/movies/upcoming"
-                        className={({ isActive }) =>
-                            isActive ? activeNav : inactiveNav
-                        }
-                    >
-                        Upcoming
-                    </NavLink>
-                </li>
+                {navLinks.map((navLink) => (
+                    <li key={navLink.title}>
+                        <NavLink
+                            to={navLink.title}
+                            className={({ isActive }) =>
+                                isActive ? activeNav : inactiveNav
+                            }
+                        >
+                            {navLink.title}
+                        </NavLink>
+                    </li>
+                ))}
             </ul>
         </div>
     );
