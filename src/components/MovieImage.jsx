4c82dce571dab88b73bdf05a42f9a689d44384f1
Shaukat Ali <Shaukatalibans@gmail.com>
2025-02-13 11:30:49 +0500

    Moved Images to images folder, update img tag to have proper width

--- src/components/MovieImage.jsx
+++ src/components/MovieImage.jsx
@@ -1,11 +1,12 @@
-import imgBackup from "../assets/405-image.jpg";
+import noImage from "../assets/images/405-image.jpg";
 
 export const MovieImage = ({ path = "", title = "", className = "" }) => {
-    const image = path ? `https://image.tmdb.org/t/p/w500/${path}` : imgBackup;
+    const image = path ? `https://image.tmdb.org/t/p/w500/${path}` : noImage;
 
     return (
         <img
-            className={`rounded-t-lg ${className}`}
+            width="500"
+            className={`w-500 h-auto rounded-t-lg ${className}`}
             src={image}
             alt={title || "405 image"}
         />
