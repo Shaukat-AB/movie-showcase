334fb6f8e3ab6c94c1eea27c2964ab6c720640e7
Shaukat Ali <Shaukatalibans@gmail.com>
2025-11-19 12:05:07 +0500

    update: move images and logo to public folder, remove old assests folder

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,81 +1,73 @@
-import { NavBar } from "./NavBar";
-import { Link, useNavigate } from "react-router";
-import { useState } from "react";
-import { SearchField } from "./SearchField";
-import { useLocalDarmode } from "../../hooks/useLocalDarmode";
-import { MenuIcon, MoonIcon, SunIcon, SearchIcon } from "../../lib";
-import Logo from "../../assets/movie-logo.svg";
+import { NavBar } from './NavBar';
+import { Link, useNavigate } from 'react-router';
+import { useState } from 'react';
+import { SearchField } from './SearchField';
+import { useLocalDarmode } from '../../hooks/useLocalDarmode';
+import { MenuIcon, MoonIcon, SunIcon, SearchIcon } from '../../lib';
+import Logo from '/movie-logo.svg';
 
 export const Header = () => {
-    const [open, setOpen] = useState(false);
-    const [darkmode, setDarkmode] = useLocalDarmode();
-    const navigate = useNavigate();
-    const onSearch = (value) => {
-        navigate(`/search?q=${value}`);
-    };
+  const [open, setOpen] = useState(false);
+  const [darkmode, setDarkmode] = useLocalDarmode();
+  const navigate = useNavigate();
 
-    return (
-        <header>
-            <nav className="bg-white border-b-2 border-gray-200 dark:bg-gray-900 dark:border-slate-900">
-                <div className="max-w-(--breakpoint-xl) flex flex-wrap items-center justify-between mx-auto p-4">
-                    <Link
-                        to="/"
-                        className="flex items-center space-x-3 rtl:space-x-reverse"
-                    >
-                        <img src={Logo} className="h-8" alt="Movie Showcase" />
-                        <span className="hidden lg:inline-block self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                            Movie Showcase
-                        </span>
-                    </Link>
-                    <div className="flex md:order-2 sm:ml-0 ml-auto">
-                        <button
-                            type="button"
-                            className="mx-4 inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                            onClick={() => setDarkmode(!darkmode)}
-                        >
-                            <span className="sr-only">
-                                Change Theme Light/Dark
-                            </span>
-                            {!darkmode ? (
-                                <SunIcon className="w-5 h-5" />
-                            ) : (
-                                <MoonIcon />
-                            )}
-                        </button>
+  const onSearch = (value) => {
+    navigate(`/search?q=${value}`);
+  };
 
-                        <button
-                            type="button"
-                            data-collapse-toggle="navbar-search"
-                            aria-controls="navbar-search"
-                            aria-expanded="false"
-                            className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-hidden focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1"
-                            onClick={() => setOpen(() => !open)}
-                        >
-                            <SearchIcon className="w-5 h-5" />
-                            <span className="sr-only">Search</span>
-                        </button>
-                        {/* Search Field for Medium or larger screens*/}
+  return (
+    <header>
+      <nav className="bg-white border-b-2 border-gray-200 dark:bg-gray-900 dark:border-slate-900">
+        <div className="max-w-(--breakpoint-xl) flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link
+            to="/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img src={Logo} className="h-8" alt="Movie Showcase" />
+            <span className="hidden lg:inline-block self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Movie Showcase
+            </span>
+          </Link>
+          <div className="flex md:order-2 sm:ml-0 ml-auto">
+            <button
+              type="button"
+              className="mx-4 inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              onClick={() => setDarkmode(!darkmode)}
+            >
+              <span className="sr-only">Change Theme Light/Dark</span>
+              {!darkmode ? <SunIcon className="w-5 h-5" /> : <MoonIcon />}
+            </button>
 
-                        <SearchField
-                            onSearch={onSearch}
-                            className="hidden md:block"
-                        />
-                        <button
-                            data-collapse-toggle="navbar-search"
-                            type="button"
-                            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-hidden focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                            aria-controls="navbar-search"
-                            aria-expanded="false"
-                            onClick={() => setOpen(!open)}
-                        >
-                            <span className="sr-only">Open main menu</span>
-                            <MenuIcon className="w-5 h-5" />
-                        </button>
-                    </div>
+            <button
+              type="button"
+              data-collapse-toggle="navbar-search"
+              aria-controls="navbar-search"
+              aria-expanded="false"
+              className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-hidden focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 me-1"
+              onClick={() => setOpen(() => !open)}
+            >
+              <SearchIcon className="w-5 h-5" />
+              <span className="sr-only">Search</span>
+            </button>
+            {/* Search Field for Medium or larger screens*/}
 
-                    <NavBar open={open} onSearch={onSearch} />
-                </div>
-            </nav>
-        </header>
-    );
+            <SearchField onSearch={onSearch} className="hidden md:block" />
+            <button
+              data-collapse-toggle="navbar-search"
+              type="button"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-hidden focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-search"
+              aria-expanded="false"
+              onClick={() => setOpen(!open)}
+            >
+              <span className="sr-only">Open main menu</span>
+              <MenuIcon className="w-5 h-5" />
+            </button>
+          </div>
+
+          <NavBar open={open} onSearch={onSearch} />
+        </div>
+      </nav>
+    </header>
+  );
 };
--- src/components/Movie/MovieImage.jsx
+++ src/components/Movie/MovieImage.jsx
@@ -1,14 +1,14 @@
-import noImage from "../../assets/images/405-image.jpg";
+import noImage from '/images/405-image.jpg';
 
-export const MovieImage = ({ path = "", title = "", className = "" }) => {
-    const image = path ? `https://image.tmdb.org/t/p/w500/${path}` : noImage;
+export const MovieImage = ({ path = '', title = '', className = '' }) => {
+  const image = path ? `https://image.tmdb.org/t/p/w500/${path}` : noImage;
 
-    return (
-        <img
-            width="500"
-            className={`w-500 font- h-auto rounded-t-lg ${className}`}
-            src={image}
-            alt={title || "405 image"}
-        />
-    );
+  return (
+    <img
+      width="500"
+      className={`w-500 font- h-auto rounded-t-lg ${className}`}
+      src={image}
+      alt={title || '405 image'}
+    />
+  );
 };
