344ca1efb68c3ba5e4a2e9077b48aecf6009e2e3
Shaukat Ali <Shaukatalibans@gmail.com>
2025-11-19 13:15:12 +0500

    fix: fix where empty p tag in search page causes first movie card to unaligned from rest on smaller screen

--- src/pages/SearchResult/index.jsx
+++ src/pages/SearchResult/index.jsx
@@ -1,28 +1,31 @@
-import { useSearchParams } from "react-router";
-import { MovieCard, PageWrapper } from "../../components";
-import { useSearchMovie } from "../../hooks/useSearchMovie";
+import { useSearchParams } from 'react-router';
+import { MovieCard, PageWrapper } from '../../components';
+import { useSearchMovie } from '../../hooks/useSearchMovie';
 
 export const SearchResult = () => {
-    const [params] = useSearchParams();
-    const query = params.get("q");
-    const [movies, isLoading, error] = useSearchMovie(query);
+  const [params] = useSearchParams();
+  const query = params.get('q');
+  const [movies, isLoading, error] = useSearchMovie(query);
 
-    return (
-        <PageWrapper
-            pagetitle={`Results for '${query}'`}
-            errorMsg={error}
-            isLoading={isLoading}
-        >
-            <section className="max-w-7xl mx-auto py-8 flex flex-wrap justify-center xl:justify-start">
-                <p className="p-8 mt-12 flex-1 text-center text-3xl text-gray-700 dark:text-gray-100 break-all">
-                    {movies?.length === 0
-                        ? `No results found for '${query}'`
-                        : ""}
-                </p>
-                {movies?.map((movie) => (
-                    <MovieCard key={movie.id} movie={movie} />
-                ))}
-            </section>
-        </PageWrapper>
-    );
+  return (
+    <PageWrapper
+      pagetitle={`Results for '${query}'`}
+      errorMsg={error}
+      isLoading={isLoading}
+    >
+      <section className="max-w-7xl mx-auto py-8 flex flex-wrap justify-center xl:justify-start">
+        {movies?.length === 0 ? (
+          <p className="p-8 mt-12 flex-1 text-center text-3xl text-gray-700 dark:text-gray-100 break-all">
+            {`No results found for '${query}'`}
+          </p>
+        ) : (
+          ''
+        )}
+
+        {movies?.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </section>
+    </PageWrapper>
+  );
 };
