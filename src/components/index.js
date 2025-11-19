17ecb01148b8a82e91e15c33a5d8fdebe942b57f
Shaukat Ali <Shaukatalibans@gmail.com>
2025-11-19 12:08:38 +0500

    update: index.jsx to index.js where no actual jsx used

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+export { Header } from './Header/Header';
+export { Footer } from './Footer/Footer';
+
+export { MovieCard } from './Movie/MovieCard';
+export { MovieImage } from './Movie/MovieImage';
+export { SearchField } from './Header/SearchField';
+
+export { PageWrapper } from './PageWrapper';
+export { Spinner } from './Spinner';
+export { ErrorMsg } from './ErrorMsg';
