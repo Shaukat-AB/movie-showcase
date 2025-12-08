52acd5c310adc3d701871818b79ab78c8a4c3813
Shaukat Ali <Shaukatalibans@gmail.com>
2025-12-08 17:57:17 +0500

    update: rename hooks to use .js not .jsx

--- src/hooks/useGetMovieById.jsx
+++
@@ -1,8 +0,0 @@
-import { createMovieByIdReqURL } from "../api";
-import { useAxios } from "./useAxios";
-
-export const useGetMovieById = (id) => {
-    const [movie, isLoading, error] = useAxios(createMovieByIdReqURL(id));
-
-    return [movie, isLoading, error];
-};
